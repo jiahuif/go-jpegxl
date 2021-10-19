409761828d8ae602e592248ca0dd40e096e83656
Indeed <indeed@indeed.moe>
2021-10-18 19:00:00 -0700

    add instructions on regenerate binding code.

--- internal/decoder/glue/lib.go
+++ internal/decoder/glue/lib.go
@@ -1,7 +1,10 @@
+// Package glue generates Go bindings to libjxl.
+// Please run "go generate" every time you modify decoder.i
+
 //go:generate swig -c++ -go -cgo -intgosize 64 decoder.i
 package glue
 
 /*
 #cgo pkg-config: libjxl
- */
+*/
 import "C"
